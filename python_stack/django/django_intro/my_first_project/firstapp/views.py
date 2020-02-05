bd56e9b9a2e0d06c1539bdbd3e2a218da0180e1a
napatpiya <napat_ont@hotmail.com>
2020-02-05 16:19:05 -0500

    Set django call index.html file

--- python_stack/django/django_intro/my_first_project/firstapp/views.py
+++ python_stack/django/django_intro/my_first_project/firstapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    return HttpResponse("placeholder to later display a list of all blogs")
+    return render(request, "index.html")
 
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
