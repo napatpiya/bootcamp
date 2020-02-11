2e4971be2fc0cf2a5e6d29897a6b43eb27e783c4
napatpiya <napat_ont@hotmail.com>
2020-02-11 14:33:04 -0500

    Upload one to many database with Django

---
+++ python_stack/django/django_orm/one_to_many_project/one_to_many_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OneToManyAppConfig(AppConfig):
+    name = 'one_to_many_app'
--- python_stack/django/django_orm/one_to_many_project/one_to_many_app/models.py
+++ python_stack/django/django_orm/one_to_many_project/one_to_many_app/models.py
@@ -5,8 +5,9 @@ class Dojos(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField()
 
 class Ninjas(models.Model):
-    dojo_id = models.ForeignKey(Dojos, related_name="dojo_id")
+    dojo_id = models.ForeignKey('Dojos', on_delete = models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
