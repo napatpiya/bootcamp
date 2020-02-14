4a1fe0ad90798b73c527464c76bd9b05c44955ef
napatpiya <napat_ont@hotmail.com>
2020-02-14 14:37:51 -0500

    Upload amadon project

---
+++ python_stack/django/django_full_stack/amadon-master/poorly_coded_store/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Order, Product
+
+def index(request):
+    request.session['qtt'] = 0
+    request.session['sum'] = 0.0
+    context = {
+        "all_products": Product.objects.all()
+    }
+    return render(request, "store/index.html", context)
+
+def checkout(request):
+    context = {
+        'total': request.session['total'],
+        'qtt': request.session['qtt'],
+        'sum': request.session['sum']
+    }
+    return render(request, "store/checkout.html", context)
+
+def calc(request):
+    print(request.POST)
+    quantity_from_form = int(request.POST["quantity"])
+    price_from_form = float(request.POST["price"])
+    total_charge = quantity_from_form * price_from_form
+    request.session['total'] = total_charge
+    request.session['qtt'] += int(request.POST["quantity"])
+    request.session['sum'] += total_charge
+    print("Charging credit card...")
+    #Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect("/checkout")
