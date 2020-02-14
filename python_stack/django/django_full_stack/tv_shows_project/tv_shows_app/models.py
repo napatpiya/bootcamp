73b13f4334ff0325aba38f075f9733137abcc80d
napatpiya <napat_ont@hotmail.com>
2020-02-14 13:14:02 -0500

    Update tv_shows_project:  - Add validation on fields  - Check existing title before create or edit

--- python_stack/django/django_full_stack/tv_shows_project/tv_shows_app/models.py
+++ python_stack/django/django_full_stack/tv_shows_project/tv_shows_app/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class ValiManager(models.Manager):
+    def validator(self, postData, request):
+        errors = {}
+        print(datetime.date.today())
+        print(postData['reldate'])
+        present = datetime.date.today()
+        reldate = datetime.datetime.strptime(postData['reldate'], "%Y-%m-%d").date()
+        print(reldate > present)
+        if len(postData['title']) < 2:
+            errors["title"] = "Title name should be at least 2 characters"
+        if request.session['check'] == 1:
+            errors["title"] = "The title already exist in the database"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network name should be at least 3 characters"
+        if postData['reldate'] == '':
+            errors['reldate'] = "Release Date should not be null"
+        if reldate > present:
+            errors['reldate'] = "Release Date should be the past"
+        if len(postData['desc']) != 0 and len(postData['desc']) < 10:
+            errors['desc'] = "Description name should be at least 10 characters (Optional)"
+        return errors
+
 class TvShows(models.Model):
     title = models.CharField(max_length=45)
     network =  models.CharField(max_length=20)
@@ -8,3 +31,4 @@ class TvShows(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ValiManager()
--- python_stack/django/django_full_stack/tv_shows_project/tv_shows_app/views.py
+++ python_stack/django/django_full_stack/tv_shows_project/tv_shows_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, HttpResponse
 from tv_shows_app.models import TvShows
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return redirect("/shows")
 
 def shows(request):
+    request.session['title'] = ''
+    request.session['network'] = ''
+    request.session['reldate'] = ''
+    request.session['desc'] = ''
     context = {
         'show': TvShows.objects.all()
     }
@@ -29,21 +34,49 @@ def editshow(request, id):
     return render(request, "updateshow.html", context)
 
 def createshow(request):
-    print(request.POST)
-    TvShows.objects.create(title=request.POST['title'], network=request.POST['network'], releasedate=request.POST['reldate'], description=request.POST['desc'])
-    last = TvShows.objects.last()
-    id = last.id
-    return redirect(f"/shows/{id}")
+    #print(request.POST)
+    request.session['check'] = 0
+    for name in TvShows.objects.all():
+        if request.POST['title'] == name.title:
+            request.session['check'] = 1
+    errors= TvShows.objects.validator(request.POST, request)
+    request.session['title'] = request.POST['title']
+    request.session['network'] = request.POST['network']
+    request.session['reldate'] = request.POST['reldate']
+    request.session['desc'] = request.POST['desc']
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+        TvShows.objects.create(title=request.POST['title'], network=request.POST['network'], releasedate=request.POST['reldate'], description=request.POST['desc'])
+        last = TvShows.objects.last()
+        id = last.id
+        messages.success(request, "")
+        return redirect(f"/shows/{id}")
 
 def updateshow(request, id):
-    print(request.POST)
-    show = TvShows.objects.get(id=id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.releasedate = request.POST['reldate']
-    show.description = request.POST['desc']
-    show.save()
-    return redirect(f"/shows/{id}")
+    #print(request.POST)
+    request.session['check'] = 0
+    oldtitle = TvShows.objects.get(id=id)
+    if request.POST['title'] != oldtitle.title:
+        for name in TvShows.objects.all():
+            if request.POST['title'] == name.title:
+                request.session['check'] = 1
+    errors= TvShows.objects.validator(request.POST, request)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{id}/edit")
+    else:
+        show = TvShows.objects.get(id=id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.releasedate = request.POST['reldate']
+        show.description = request.POST['desc']
+        show.save()
+        messages.success(request, "")
+        return redirect(f"/shows/{id}")
 
 def destroyshow(request, id):
     show = TvShows.objects.get(id=id)
