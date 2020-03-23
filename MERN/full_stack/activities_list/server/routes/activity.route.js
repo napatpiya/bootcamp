31c6f46fe98aff8dfcedecc117370768cdebb2e7
napatpiya <napat_ont@hotmail.com>
2020-03-22 22:46:43 -0400

    Update activities list practice  - add user db and validation authen

--- MERN/full_stack/activities_list/server/routes/activity.route.js
+++ MERN/full_stack/activities_list/server/routes/activity.route.js
@@ -1,6 +1,10 @@
 const ActivityController = require('../controllers/activity.controller');
+const UserController = require('../controllers/user.controller');
 
 module.exports = function(app){
+    app.post('/api/register', UserController.register);
+    app.post('/api/login', UserController.login);
+    
     app.get('/api', ActivityController.index);
     app.post('/api/activities', ActivityController.create);
     app.get('/api/activities', ActivityController.getAll);
--- MERN/full_stack/activities_list/server/server.js
+++ MERN/full_stack/activities_list/server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 require('./config/mongoose.config'); // This is new
 
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json()); // This is new
 app.use(express.urlencoded({ extended: true })); // This is new
 
