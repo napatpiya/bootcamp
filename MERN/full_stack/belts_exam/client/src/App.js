12f64174bd7352faa7681427c3be36eb281e91c1
napatpiya <napat_ont@hotmail.com>
2020-03-23 14:13:59 -0400

    React Belts Exam

--- MERN/full_stack/belts_exam/client/src/App.js
+++ MERN/full_stack/belts_exam/client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-import Main from './views/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PetsList from './components/PetsList';
+import CreatPet from './views/CreatePet';
+import { Router } from '@reach/router';
+import DetailPet from './views/DetailPet';
+import EditPet from './views/EditPet';
+import SignIn from './views/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
+    <div className="App container">
+      <Router>
+        <SignIn path="/signin" />
+        <PetsList path="/" />
+        <CreatPet path="/new" />
+        <DetailPet path="/pets/:_id" />
+        <EditPet path="/pets/:_id/edit" />
+      </Router>
     </div>
   );
 }
--- MERN/full_stack/belts_exam/server/server.js
+++ MERN/full_stack/belts_exam/server/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const db_name = ;
+const db_name = "petDB";
 const port = 8000;
+// const cookieParser = require('cookie-parser');
 
-// require('./config/mongoose.config')(db_name); // This is new
+require('./config/mongoose.config')(db_name);
 
+// app.use(cookieParser());
+// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(cors());
-app.use(express.json()); // This is new
-app.use(express.urlencoded({ extended: true })); // This is new
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// require('./routes/activity.route')(app);
+require('./routes/pet.route')(app);
 
 app.listen(port, () => {
     console.log(`Listening at Port ${port}`)
